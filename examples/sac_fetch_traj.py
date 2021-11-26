14719ce478ea4b1f9a7cf28ef2284e6c598672ca
Yongseok Kwon <kwonys@umich.edu>
2021-11-26 01:11:23 -0500

    fix hyperparameter of gym_fetch

--- examples/sac_fetch_traj.py
+++ examples/sac_fetch_traj.py
@@ -88,12 +88,12 @@ if __name__ == "__main__":
         layer_size=256,
         replay_buffer_size=int(1E6),
         algorithm_kwargs=dict(
-            num_epochs=3000,
-            num_eval_steps_per_epoch=5000,
-            num_trains_per_train_loop=1000,
-            num_expl_steps_per_train_loop=1000,
-            min_num_steps_before_training=1000,
-            max_path_length=1000,
+            num_epochs=5000,
+            num_eval_steps_per_epoch=25000,
+            num_trains_per_train_loop=5000,
+            num_expl_steps_per_train_loop=5000,
+            min_num_steps_before_training=5000,
+            max_path_length=5000,
             batch_size=256,
         ),
         trainer_kwargs=dict(
